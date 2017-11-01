f725e35b3d7ca8cb601486470d9e6ab27db291ca
1160457037@qq.com <yu266317>
2017-11-02 07:55:00 +0800

    这是一个新包

---
+++ app/main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Component1 from './component/Component1.js';
+
+ReactDom.render(<Component1/>,document.getElementById('content'));
