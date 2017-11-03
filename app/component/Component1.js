f23d187fdb135af7bfe86a8eafe27f7eb5a3eca5
1160457037@qq.com <yu266317>
2017-11-03 08:16:10 +0800

    10

--- app/component/Component1.js
+++ app/component/Component1.js
@@ -1,10 +1,29 @@
 //Component1.jsx
 import React from 'react';
+import Chance from 'chance';
 
 class Component1 extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            name:chance.first(),
+            country:chance.country({full:true})
+        }
+    }
+    bottonClicked(){
+        console.log("the botton is clicked!");
+        this.forceUpdate();
+    }
     render() {
         return (
-            <div>恭喜老二跑通react+webpack+babel+webpack-server-dev!!!</div>
+            <div>
+                <p>恭喜老二跑通react+webpack+babel+webpack-server-dev!!!</p>
+                <p>{this.props.message}</p>
+                <p>{this.state.name}</p>
+                <p><span>国家：</span><span>{this.state.country}</span></p>
+                <botton onClick={this.bottonClicked.bind(this)}>点击</botton>
+            </div>
+
         )
     }
 }
--- app/main.js
+++ app/main.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Component1 from './component/Component1.js';
 
-ReactDom.render(<Component1/>,document.getElementById('content'));
+ReactDom.render(<Component1 message="This is coming from props!"/>,document.getElementById('content'));
