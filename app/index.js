8ea5a93a60961d40ea4ed8c9d801758eb446115d
1160457037@qq.com <yu266317>
2018-04-17 20:20:46 +0800

    demo

--- app/index.js
+++ app/index.js
@@ -5,11 +5,7 @@ import ReactDom from 'react-dom';
 
 import App from './components/App';
 
-var comments = [{
-    "author": "宋玉清", "date": "5分钟前", "text": "今个天气很好啊！一起出去浪啊！"
-}, {
-    "author": "何小三", "date": "3分钟前", "text": "对对对，适合出去打一炮！"
-}]
+
 ReactDom.render(
     <App url="comments.json"/>,
     document.getElementById('app')
